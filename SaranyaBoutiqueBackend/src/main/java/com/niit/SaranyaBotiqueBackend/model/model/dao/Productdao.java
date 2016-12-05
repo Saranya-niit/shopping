0ae7110be6528af2eb305051d336dfa66fa11410
Saranya-niit <saranyasubhash76@gmail.com>
2016-12-05 10:07:55 +0530

    this is the ui of saranya botique back end

--- SaranyaBoutiqueBackend/src/main/java/com/niit/SaranyaBotiqueBackend/model/model/dao/Productdao.java
+++ SaranyaBoutiqueBackend/src/main/java/com/niit/SaranyaBotiqueBackend/model/model/dao/Productdao.java
@@ -7,5 +7,8 @@ import com.niit.SaranyaBoutiqueBackend.model.Product;
 
 public interface Productdao {
 	public List<Product>getAllProduct();
+	
 	public Product getById(int id); 
+	public void addProduct(Product p);
+	public void updateProduct(Product p);
 }
